24a271a253ce199f39b773e3cafdbdcf3fa5a968
jslusarczykk <jan.slusarczykk@gmail.com>
2024-10-29 22:54:43 +0100

    Dodaj pliki projektów.

---
+++ Simulator/Creature.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulator
+{
+    internal class Creature
+    {
+        public string Name { get; set; }
+        public int Level { get; set; }
+        public string Info => $"{Name} [{Level}]";
+
+        public Creature(string name, int level = 1)
+        {
+            Name = name;
+            Level = level;
+        }
+
+        public Creature() { }
+
+        public void SayHi()
+        {
+            Console.WriteLine($"Hi, I'm {Name}, my level is {Level}.");
+        }
+    }
+
+}
